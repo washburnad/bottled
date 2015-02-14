873e8e38c5c3aa0d2a7b921fb3d081dbecbd87b9
Aaron Washburn <aaron@it-kutch.com>
2015-02-14 10:52:22 -0500

    Initial Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bottled::Application.config.secret_key_base = '93df34cdab0955e3c0e31da01f646edddee73a1b546dc2efdb70d5cde1b224fbaf2d000bc4e143a9c9ca9d3301e7f895be22aca3b35fe8d31a0495c9ae90da0d'
