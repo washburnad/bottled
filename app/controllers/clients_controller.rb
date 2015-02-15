f740901857257c3557aa4df2ad56815de7100d99
Aaron Washburn <aaron@it-kutch.com>
2015-02-15 11:03:39 -0500

    Create client CRUD

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,7 +2,7 @@ class ClientsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-
+		@clients = Client.all
 	end
 
 	def new
@@ -10,19 +10,36 @@ class ClientsController < ApplicationController
 	end
 
 	def create
-
+		@client = Client.create(client_params)
+		if @client.valid?
+			redirect_to client_path(@client)
+		else
+			render :new, :status => :unprocessable_entity
+		end		
 	end
 
 	def show
-
+		@client = Client.find(params[:id])
 	end
 
 	def edit
-
+		@client = Client.find(params[:id])
 	end
 
 	def update
+		@client = Client.find(params[:id])
+		@client.update_attributes(client_params)
+		if @client.valid?
+			redirect_to client_path(@client)
+		else
+			render :new, :status => :unprocessable_entity
+		end
+	end
 
+	private
+
+	def client_params
+		params.require(:client).permit(:name, :contact_name, :contact_email, :contact_title, :contact_phone)
 	end
 
 end
