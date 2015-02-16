c1ea5414386b9e29f82764c781c330aa92b32c8e
Aaron Washburn <aaron@it-kutch.com>
2015-02-16 16:44:35 -0500

    Event start creates event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,13 @@
 class EventsController < ApplicationController
+
+	def create
+		@event = Event.create(event_params)
+		render :text => 'updated'
+	end
+
+	private
+
+	def event_params
+		params.require(:event).permit(:name, :description, :start_time, :end_time)
+	end
 end
