ff60aa1798f081c62a5e08395f80f248e076826e
Aaron Washburn <aaron@it-kutch.com>
2015-02-17 16:54:24 -0500

    Refactor display of projects and task on index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
 
 	def index
 		@clients = current_user.clients.order("updated_at").limit(3)
-		@projects = current_user.projects.all
-		@tasks = current_user.tasks.all
+		@projects = current_user.projects.order("updated_at").limit(3)
+		@tasks = current_user.tasks.order("updated_at").limit(3)
 	end
+
 end
