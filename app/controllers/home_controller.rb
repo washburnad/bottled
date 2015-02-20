6a6b326f4e1b4e006d5080fba77963375b95ae67
Aaron Washburn <aaron@it-kutch.com>
2015-02-20 13:19:42 -0500

    Move javascript and fix bugs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
 
 	def index
-		@clients = current_user.clients.order("updated_at").limit(3)
-		@projects = current_user.projects.order("updated_at").limit(3)
-		@tasks = current_user.tasks.order("updated_at").limit(3)
+		if current_user
+			@clients = current_user.clients.order("updated_at").limit(3)
+			@projects = current_user.projects.order("updated_at").limit(3)
+			@tasks = current_user.tasks.order("updated_at").limit(3)
+		end
 	end
 
 end
