71c01c02581cd5cc556eb8290c9f38c3e7e3257c
Aaron Washburn <aaron@it-kutch.com>
2015-02-21 12:35:55 -0500

    Continue styling, add date format helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+	# helper_method :time_to_string
+	# convert time in seconds to a string
+	def time_to_string(time)
+		if !time.nil?
+			Time.at(time)
+		else
+			"Unknown"
+		end
+	end
+
+	# helper_method :time_in_h_mm
+	# converts seconds to a H:MM
+	def time_in_h_mm(seconds)
+		if !seconds.nil?
+			Time.at(seconds).utc.strftime("%H:%M")
+		else
+			"00:00"
+		end
+	end		
+
 end
