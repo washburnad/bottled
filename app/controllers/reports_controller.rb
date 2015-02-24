4bb498c306cd3538d6fafdda3e4360f1b870112f
Aaron Washburn <aaron@it-kutch.com>
2015-02-23 20:27:33 -0500

    Add client and project selection partials and start ajax query

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,23 @@
 class ReportsController < ApplicationController
 	def index
 		@clients = Client.all
+		@projects = Project.all
 		@type = type_param[:type]
 	end
 
+
+	def query
+		puts query_params
+		if query_params[:client_id]
+			@projects = Client.find( query_params[:client_id]).projects.all
+			render :json => '@projects'
+		elsif query_params[:project_id]
+			@tasks = Project.find( query_params[:project_id]).tasks.all 
+			render :json => '@tasks'
+		else
+			render :text => 'unprocessable_entity', :status => :unprocessable_entity
+		end
+	end
 
 	private
 
@@ -18,9 +32,18 @@ class ReportsController < ApplicationController
 		return options
 	end
 
+	
+	helper_method :show_projects
+	def show_projects
+		return true
+	end
+
+	def query_params
+		@query_params = params.permit( :client_id, :project_id )
+	end
 
 	def type_param
-		type = params.permit(:type)
+		@type_param = params.permit(:type)
 	end
 
 
