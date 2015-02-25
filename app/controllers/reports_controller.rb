9835351bc4d30f9b695d87ac827cd4a5534d5474
Aaron Washburn <aaron@it-kutch.com>
2015-02-25 11:22:49 -0500

    Ensure user authentication for reports and filter by user

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,11 @@
 class ReportsController < ApplicationController
+	before_action :authenticate_user!
+
 	def index
-		@clients = Client.all
+		@clients = current_user.clients.all
 		@type = report_params[:type]
 		if report_params[:client_id]
-			@projects = Client.find( report_params[:client_id] ).projects.all
+			@projects = current_user.clients.find( report_params[:client_id] ).projects.all
 
 			respond_to do |format|
 			  format.html { render :partial => 'projects_select' }
