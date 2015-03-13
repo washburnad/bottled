1c4e804e4883eef6e914122971f19499eefc3f85
Aaron Washburn <aaron@it-kutch.com>
2015-03-13 10:23:24 -0400

    Add games controller tests

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,6 @@
 class ClientsController < ApplicationController
 	before_action :authenticate_user!
+	before_action :require_authorized_for_current_client, only: [ :show, :edit, :udpate]
 
 	def index
 		@clients = current_user.clients.to_a
@@ -40,6 +41,18 @@ class ClientsController < ApplicationController
 
 	def client_params
 		params.require(:client).permit(:name, :contact_name, :contact_email, :contact_title, :contact_phone, :glyph_id, :glyph_color)
+	end
+
+	helper_method :current_client
+	def current_client
+		@current_client ||= Client.find(params[:id])
+	end
+
+
+	def require_authorized_for_current_client
+		if current_client.user != current_user
+			redirect_to root_path, alert: 'Current user does not have access to that page.'
+		end
 	end
 
 end
--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -27,4 +27,86 @@ class ClientsControllerTest < ActionController::TestCase
     get :edit, id: client.id
     assert :success
   end
+
+  test "should fail show and edit when not signed in" do
+    client = FactoryGirl.create(:client, user_id: @user.id)
+    sign_out @user
+
+    get :show, id: client.id
+    assert_redirected_to new_user_session_path
+
+    get :edit, id: client.id
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should fail show and edit when wrong user" do
+    user2 = FactoryGirl.create(:user)
+    client = FactoryGirl.create(:client, user_id: user2.id)
+
+    get :show, id: client.id
+    assert_redirected_to root_path
+
+    get :edit, id: client.id
+    assert_redirected_to root_path
+
+  end
+
+  test "should create new client" do
+    assert_difference('Client.count') do
+      post :create, client: {name: 'A client'}
+    end
+    assert :success
+    assert_redirected_to client_path(assigns(:client))
+  end 
+
+  test "should fail create when not signed in" do
+    sign_out @user
+
+    assert_no_difference('Client.count') do
+      post :create, client: {name: 'A client'}
+    end
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update client" do
+    create_client
+    put :update, id: @client.id, client: {name: 'New name'}
+    @client.reload
+    
+    assert_equal 'New name', @client.name
+    assert :success
+    assert_redirected_to client_path(@client)
+  end
+
+  test "should fail update with wrong user" do
+    create_client
+    sign_out @user
+    user2 = FactoryGirl.create(:user)
+    sign_in user2
+    
+    puts @user
+    puts user2
+
+    put :update, id: @client.id, client: {name: 'New name'}
+    @client.reload
+    
+    assert_not_equal 'New name', @client.name
+    assert_redirected_to client_path(@client)
+  end
+
+  test "should fail update when signed out" do
+    create_client
+    sign_out @user
+
+    put :update, id: @client.id, client: {name: 'New name'}
+    @client.reload
+    
+    assert_not_equal 'New name', @client.name
+    assert_redirected_to new_user_session_path
+  end
+
+  def create_client
+    post :create, client: {name: 'A client'}
+    @client = Client.last
+  end
 end
