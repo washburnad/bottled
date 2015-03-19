52dfd05e689cb39110911ed3c99fa411b2d01ea7
Aaron Washburn <aaron@it-kutch.com>
2015-03-19 18:03:56 -0400

    Show events on reports#show page, update associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
-	belongs_to :project
+  belongs_to :task
+	has_one :project, through: :events
+  has_one :client, through: :projects
   has_many :reports, as: :reportable
 
 	validates :start_time, :presence => true
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
 	belongs_to :project
 	has_many :events
+  has_one :client, through: :project
   has_many :reports, as: :reportable
 
 	validates :name, :presence => true
