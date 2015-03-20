dc673ea3ffa3f8ad275af1e4fed734d60b574591
Aaron Washburn <aaron@it-kutch.com>
2015-03-20 09:53:08 -0400

    Display report total_duration

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,15 @@ class Report < ActiveRecord::Base
   belongs_to :reportable, polymorphic: true
   belongs_to :user
 
+
+  def total_duration
+    total = 0
+    self.reportable.events.each do |e|
+      if e.duration.present?
+        total += e.duration
+      end
+    end
+    return total
+  end
+
 end
