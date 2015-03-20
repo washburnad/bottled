130914a9e78408abed6eb2a487b966e5fe08a628
Aaron Washburn <aaron@it-kutch.com>
2015-03-20 09:05:31 -0400

    Refactor stylesheet and style report

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ActiveRecord::Base
 	belongs_to :project
 	has_many :events
   has_one :client, through: :project
+  has_one :user, through: :project
+  
   has_many :reports, as: :reportable
 
 	validates :name, :presence => true
