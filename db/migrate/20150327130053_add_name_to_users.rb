65bb1d6a977f0384e390fb33a8e9153648b23d42
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-03-27 09:04:32 -0400

    run migration

--- db/migrate/20150327130053_add_name_to_users.rb
+++ db/migrate/20150327130053_add_name_to_users.rb
@@ -2,7 +2,7 @@ class AddNameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
+    add_index :users, :last_name
   end
 
-  add_index :users, :last_name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326185643) do
+ActiveRecord::Schema.define(version: 20150327130053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,9 +100,12 @@ ActiveRecord::Schema.define(version: 20150326185643) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["last_name"], name: "index_users_on_last_name", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
