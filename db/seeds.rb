6243ea45baba00fa8afe183081477bc9c560f81b
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-04-20 13:22:08 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # create users
-ada = User.create!(email: 'alovelace@example.com', first_name: 'Ada', last_name: 'Lovelace', password: 'alovalace')
+ada = User.create!(email: 'alovelace@example.com', first_name: 'Ada', last_name: 'Lovelace', password: 'alovelace')
 charles = User.create!(email: 'cbabbage@example.com', first_name: 'Charles', last_name: 'Babbage', password: 'cbabbage')
 
 # create clients
@@ -26,22 +26,46 @@ diff_model = diff_eng.tasks.create!(name: 'Create Working Model')
 diff_funding = diff_eng.tasks.create!(name: 'Obtain Funding')
 diff_build = diff_eng.tasks.create!(name: 'Build Engine')
 
+# create tasks
+analyt_methodology = analytical_eng.tasks.create!(name: 'Conceptualize new analytical engine')
+analyt_design = analytical_eng.tasks.create!(name: 'Develop design of analytical engine')
+analyt_model = analytical_eng.tasks.create!(name: 'Create model engine')
+analyt_funding = analytical_eng.tasks.create!(name: 'Recruit investors')
+
+# create tasks
+
+
 # create events
 5.times do |n|
-  diff_methodology.events.create!(name: "Event#{Time.new(1822, 3, 10+n, 8)}", start_time: Time.new(1822, 3, 10+n, 8) , duration: 4)
+  diff_methodology.events.create!(name: "Event#{Time.new(1822, 3, 10+n, 8)}", start_time: Time.new(1822, 3, 10+n, 8) , duration: 5)
 end
 
-7.times do
-  diff_schematics.events.create!(name: "Event#{Time.new(1822, 3, 17+n, 8)}", start_time: Time.new(1822, 3, 17+n, 8) , duration: 4)
+7.times do |n|
+  diff_schematics.events.create!(name: "Event#{Time.new(1822, 3, 17+n, 8)}", start_time: Time.new(1822, 3, 17+n, 8) , duration: 3)
 end
 
-12.times do
+12.times do |n|
   diff_model.events.create!(name: "Event#{Time.new(1822, 4, 9+n, 8)}", start_time: Time.new(1822, 4, 9+n, 8) , duration: 4)
 end
 
-3.times do
-  diff_funding.events.create(name: "Event#{Time.new(1822, 11, 28+n, 8)}", start_time: Time.new(1822, 11, 28+n, 8) , duration: 4)
+3.times do |n|
+  diff_funding.events.create!(name: "Event#{Time.new(1822, 11, 28+n, 8)}", start_time: Time.new(1822, 11, 28+n, 8) , duration: 6 - n)
 end
 
+4.times do |n|
+  analyt_methodology.events.create!(name: "Event #{Time.new(1834, 8, 5+n, 8)}", start_time: Time.new(1834, 8, 5+n, 8) , duration: 5)
+end
+
+2.times do |n|
+  analyt_design.events.create!(name: "Event #{Time.new(1834, 9, 13+n, 8)}", start_time: Time.new(1834, 9, 13+n, 8) , duration: 2 * n)
+end
+
+5.times do |n|
+  analyt_methodology.events.create!(name: "Event #{Time.new(1834, 11, 1+n, 8)}", start_time: Time.new(1834, 11, 1+n, 8) , duration: 8 - 0.5 * n)
+end
+
+
 # create collaborations
-engine_co.collaborations.create(user: ada)
+engine_co.collaborations.create!(user: ada)
+
+
