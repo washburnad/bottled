0d7794f0b0fb38cca5312e268ca3c9006f134ea2
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-05-15 14:35:37 -0400

    Add client and report partials

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,6 @@
 module ClientsHelper
+  def show_client(client, include_glyph)
+    display = include_glyph ? glyph_for(client) : ""
+    display += link_to client.name, client_path(client) 
+  end
 end
