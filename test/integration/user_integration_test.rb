0e91940810d191910ffda36a46f0043aa0baa14c
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-06-02 13:27:16 -0400

    User and client integration tests passign

--- test/integration/user_integration_test.rb
+++ test/integration/user_integration_test.rb
@@ -8,10 +8,22 @@ class UserIntegrationTest < ActionDispatch::IntegrationTest
   end
 
   test 'sign in user' do
+    user = FactoryGirl.create(:user)
     visit new_user_session_path
-    fill_in('user_email', with: 'cbabbage@example.com')
-    fill_in('user_password', with: 'cbabbage')
-    click_button('Sign In')
-    puts current_path
+    fill_in('user_email', with: user.email)
+    fill_in('user_password', with: user.password)
+    click_link('Sign In')
+    # puts current_path
+  end
+
+  test 'click to sign up' do
+    visit new_user_session_path
+    click_on('Sign Up')
+    assert(current_path == new_user_registration_path)
+    fill_in('user_email', with: 'test@example.com')
+    fill_in('user_password', with: 'password')
+    fill_in('user_password_confirmation', with: 'password')
+    click_link('Sign Up')
+    # assert page.has_content? 'Welcome! You have signed up successfully.'
   end
 end
